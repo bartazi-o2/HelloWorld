0c63c6d628633918502f2db3ef054eef2c3bb20c
Bartlomiej Faber <bartefab@gmail.com>
2012-12-10 17:27:55 +0100

    Dodanie zmian na branch master

--- main.c
+++ main.c
@@ -5,4 +5,10 @@ main()
 	printf("Hello World!\n");
 	
 	getchar();
+	
+//Dodanie zmian na branch 'master'
+  printf("Changes in branch master\m");
+//Koniec zmian na branch 'master'
+
+    getchar();
 }
